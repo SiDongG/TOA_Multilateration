30cf91a0adb6d7c976e9fa3e574f6c93b399eefb
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-03-16 14:44:45 -0400

    Add files via upload

---
+++ Multipath_GCC.m
@@ -0,0 +1,44 @@
+%% Special Generalized CC used for testing multipath
+
+clear;clc;close all;
+fb=1e3;
+bit_period=1/fb; %bit period is 0.001 second 
+fc=1e4; %Carrier Frequency
+fs=2e4; %Sampling Rate
+bits=[1,0,1,0,0,1];
+t=0:1/(fs*fb):1/fb;
+for i=1:length(bits)
+    analog_sig=(bits(i)*2-1)*cos(2*pi*fc*t);
+    if i==1
+        analog=analog_sig;
+    else
+        analog=[analog,analog_sig];
+    end
+end
+signal=analog;
+
+path0=signal;
+path1=3*[zeros(1,5600),signal];
+path2=0.5*[zeros(1,6600),signal];
+path3=2*[zeros(1,7200),signal];
+path4=0.3*[zeros(1,9400),signal];
+signal_r=path0+path1(1:120006)+path2(1:120006)+path3(1:120006)+path4(1:120006);
+
+cross_real=fftshift(ifft(fft(signal).*conj(fft(signal))));
+subplot(2,2,1)
+plot(1:length(cross_real),cross_real);
+
+cross=fftshift(ifft(fft(signal_r).*conj(fft(signal))));
+subplot(2,2,2)
+plot(1:length(cross),cross);
+
+xcor_PHAT=1e3*abs(fftshift(ifft((fft(signal_r).*conj(fft(signal)))./(abs(fft(signal_r)).*abs(fft(signal))))));
+subplot(2,2,3)
+plot(1:length(xcor_PHAT),xcor_PHAT);
+
+xcor_SG=1e3*abs(fftshift(ifft((conj(fft(signal_r)).*fft(signal))./(abs(fft(signal)).*abs(fft(signal))))));
+subplot(2,2,4)
+plot(1:length(xcor_SG),xcor_SG);
+
+
+
