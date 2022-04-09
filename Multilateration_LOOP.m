7ac76b1624563a12ecd5681daeff1900787c568b
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-04-09 16:24:25 -0400

    Add files via upload

--- Multilateration_LOOP.m
+++ Multilateration_LOOP.m
@@ -1,16 +1,20 @@
-
+profile on 
 Error=zeros(4,5);
 y=1;
 Var=1e-4*[0.0017968    0.0122    0.032401    0.085216    0.13898]; 
-for Mode=1:4
+Measurement_X=zeros(4,5,100);
+Measurement_Y=zeros(4,5,100);
+for Mode=3:4
     index=1;
     disp(Mode)
     for position=2:2:10
         x=sqrt(position^2-y^2);
         sum=0;
         var=Var(position/2);
-        for i=1:100
+        for i=1:200
             [X,Y]=Multilateration_LLS(Mode,x,y,var);
+            Measurement_X(Mode,position/2,i)=X;
+            Measurement_Y(Mode,position/2,i)=Y;
             sum=(X-x)^2+(Y-y)^2;
         end
         Error(Mode,index)=sum;
@@ -27,4 +31,5 @@ plot(2:2:10,Error(4,:),'b-','LineWidth',1);
 
 legend('LLS1','LLS2','WLLS','2SWLLS')
 
+profile viewer
         
