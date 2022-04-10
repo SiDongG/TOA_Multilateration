b61939c7fc4b8347f92317bbd0dc0c3bab7e279b
Sidong Guo <82831509+SiDongG@users.noreply.github.com>
2022-04-10 16:30:25 -0400

    Add files via upload

--- Multilateration_Math.m
+++ Multilateration_Math.m
@@ -1,4 +1,5 @@
-function [X,Y]=Multilateration_Math(Mode,rx,ry,variance)
+function [X,Y]=Multilateration_Math(Mode,rx,ry,SNR)
+
 %Initialize Receiver Locations
 x1=0;y1=0;
 x2=1000;y2=0;
@@ -10,13 +11,19 @@ r2=sqrt((rx-x2)^2+(ry-y2)^2);
 r3=sqrt((rx-x3)^2+(ry-y3)^2);
 r4=sqrt((rx-x4)^2+(ry-y4)^2);
 %Initialize Measurement Noise
-
-Noise=sqrt(variance)*randn();
+Var1=r1^2/SNR;
+Var2=r2^2/SNR;
+Var3=r3^2/SNR;
+Var4=r4^2/SNR;
+Noise1=sqrt(Var1)*randn();
+Noise2=sqrt(Var2)*randn();
+Noise3=sqrt(Var3)*randn();
+Noise4=sqrt(Var4)*randn();
 %Measured distance
-d1=r1+Noise;
-d2=r2+Noise;
-d3=r3+Noise;
-d4=r4+Noise;
+d1=r1+Noise1;
+d2=r2+Noise2;
+d3=r3+Noise3;
+d4=r4+Noise4;
 
 % Substract Form LLS
 if Mode==1
@@ -54,10 +61,10 @@ if Mode==3
        d2^2-x2^2-y2^2;
        d3^2-x3^2-y3^2;
        d4^2-x4^2-y4^2];
-    W=(1/4)*[1/(d1^2*variance),0,0,0;
-         0,1/(d2^2*variance),0,0;
-         0,0,1/(d3^2*variance),0;
-         0,0,0,1/(d4^2*variance)];
+    W=(1/4)*[1/(d1^2*Var1),0,0,0;
+         0,1/(d2^2*Var2),0,0;
+         0,0,1/(d3^2*Var3),0;
+         0,0,0,1/(d4^2*Var4)];
     Theta=inv(A.'*W*A)*A.'*W*b;
     X=Theta(1);
     Y=Theta(2);
@@ -73,10 +80,10 @@ if Mode==4
        -2*x2, -2*y2, 1;
        -2*x3, -2*y3, 1;
        -2*x4, -2*y4, 1];
-    W=(1/4)*[1/(d1^2*variance),0,0,0;
-         0,1/(d2^2*variance),0,0;
-         0,0,1/(d3^2*variance),0;
-         0,0,0,1/(d4^2*variance)];
+    W=(1/4)*[1/(d1^2*Var1),0,0,0;
+         0,1/(d2^2*Var2),0,0;
+         0,0,1/(d3^2*Var3),0;
+         0,0,0,1/(d4^2*Var4)];
     Theta1=inv(A.'*A)*A.'*b;
     X1=Theta1(1);
     Y1=Theta1(2);
@@ -92,10 +99,10 @@ end
 % Cramer-Rao Lower Bound
 if Mode==5
     Fisher=zeros(2,2);
-    Fisher(1,1)=(rx-x1)^2/(variance*r1)+(rx-x2)^2/(variance*r2)+(rx-x3)^2/(variance*r3)+(rx-x4)^2/(variance*r4);
-    Fisher(1,2)=(rx-x1)*(ry-y1)/(variance*r1)+(rx-x2)*(ry-y2)/(variance*r2)+(rx-x3)*(ry-y3)/(variance*r3)+(rx-x4)*(ry-y4)/(variance*r4);
+    Fisher(1,1)=(rx-x1)^2/(Var1*d1)+(rx-x2)^2/(Var2*d2)+(rx-x3)^2/(Var3*d3)+(rx-x4)^2/(Var4*d4);
+    Fisher(1,2)=(rx-x1)*(ry-y1)/(Var1*d1)+(rx-x2)*(ry-y2)/(Var2*d2)+(rx-x3)*(ry-y3)/(Var3*d3)+(rx-x4)*(ry-y4)/(Var4*d4);
     Fisher(2,1)=Fisher(1,2);
-    Fisher(2,2)=(ry-y1)^2/(variance*r1)+(ry-y2)^2/(variance*r2)+(ry-y3)^2/(variance*r3)+(ry-y4)^2/(variance*r4);
+    Fisher(2,2)=(ry-y1)^2/(Var1*d1)+(ry-y2)^2/(Var2*d2)+(ry-y3)^2/(Var3*d3)+(ry-y4)^2/(Var4*d4);
     Fisher_inv=inv(Fisher);
     X=Fisher_inv(1,1);
     Y=Fisher_inv(2,2);
--- Multilateration_Mathloop.m
+++ Multilateration_Mathloop.m
@@ -1,33 +1,37 @@
+clear;clc;close all;
 Error=zeros(5,6);
-rx=100;ry=800;
-Variance=[1,10,100,1000,10000,100000,1e6,1e7];
-loop=20000;
+rx=300;ry=600;
+SNR_List=[1e-4,1e-3,1e-2,1e-1,1e0,1e1,1e2,1e3];
+loop=2000;
 for Mode=1:4
     
     disp(Mode)
-    for v=1:length(Variance)
-        Var=Variance(v);
+    for v=1:length(SNR_List)
+        SNR=SNR_List(v);
         sum=0;
+        Measurement=zeros(1,loop);
         for i=1:loop
-            [X,Y]=Multilateration_Math(Mode,rx,ry,Var);
+            [X,Y]=Multilateration_Math(Mode,rx,ry,SNR);
             sum=sum+(X-rx)^2+(Y-ry)^2;
+            Measurement(i)=X;
         end
-        Error(Mode,v)=sum/loop;
+        Error(Mode,v)=var(Measurement);
     end
 
 end  
-for v=1:length(Variance)
-    Var=Variance(v);
-    Error(5,v)=Multilateration_Math(5,rx,ry,Var);
+for v=1:length(SNR_List)
+    SNR=SNR_List(v);
+    [X,Y]=Multilateration_Math(5,rx,ry,SNR);
+    Error(5,v)=X;
 end
 
 figure;
 hold on; box on;
-plot(1:length(Variance),log(Error(1,:)),'k-','LineWidth',1);
-plot(1:length(Variance),log(Error(2,:)),'g-','LineWidth',1);
-plot(1:length(Variance),log(Error(3,:)),'r-','LineWidth',1);
-plot(1:length(Variance),log(Error(4,:)),'b-','LineWidth',1);
-plot(1:length(Variance),log(Error(5,:)),'LineWidth',1);
+plot(1:length(SNR_List),log(Error(1,:)),'k-','LineWidth',1);
+plot(1:length(SNR_List),log(Error(2,:)),'g-','LineWidth',1);
+plot(1:length(SNR_List),log(Error(3,:)),'r-','LineWidth',1);
+plot(1:length(SNR_List),log(Error(4,:)),'b-','LineWidth',1);
+plot(1:length(SNR_List),log(Error(5,:)),'LineWidth',1);
 
 
 legend('LLS1','LLS2','WLLS','2SWLLS','CRLB')
